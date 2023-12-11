1aa20b1086c182ba9d93adefcb1cfdfb35cf7a38
eblancof <edublanco.fdz@gmail.com>
2023-12-11 14:13:10 +0100

    Updated and improved main script

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Instalación en Linux
+# Práctica RTL-SDR
+En esta práctica vamos a escuchar la radio con un dispositivo RTL-SDR. Para ello vamos a utilizar el script radio.py que se encuentra en este repositorio. La idea es que el script nos permita escuchar la radio en tiempo real en la salida de audio que nosotros prefiramos. Para ello vamos a utilizar la librería pyrtlsdr que nos permite interactuar con el dispositivo RTL-SDR.
 
+Un dispositivo RTL-SDR es un dispositivo que nos permite recibir señales de radio en un rango de frecuencias de 24 MHz a 1.7 GHz. Este dispositivo es muy barato y se puede comprar en cualquier tienda online. En este caso vamos a utilizar el dispositivo RTL2832U que se puede comprar en Amazon por 30€. 
+
+![Alt text](images/image.png)
+
+Las aplicaciones de este dispositivo son prácticamente infinitas, solo estamos limitados en la tasa de muestreo que nos permite el dispositivo. Podríamos conseguir una tasa de muestreo superior si utilizamos un dispositivo Ettus USRP por ejemplo, pero estos dispositivos son mucho más caros.
+
+La máxima tasa de muestreo que admite este dispositivo es de 3.2 MS/s, lo que nos permite recibir señales de radio FM sin problemas, ya que la tasa de muestreo de la señal de radio FM es de 1.5 MHz.
+## Instalación de dependencias
 Crea un entorno virtual de conda con versión de Python 3.6
 
 ```bash
@@ -24,5 +33,5 @@ Tenemos los siguientes parámetros de configuración:
 Ejemplo de ejecución:
 
 ```
-python radio.py --freq=98000000 --gain=20
+python listen.py --freq=98000000 
 ```
--- listen.py
+++ listen.py
@@ -1,83 +1,73 @@
+import sys
 from typing import List
 from rtlsdr import RtlSdr
 import argparse
 import datetime
 import numpy as np
 import pyaudio
-import scipy.signal as signal
-import threading
+import scipy.signal as sg
+import signal
+import wave
 
 SampleStream = List[float]
 AudioStream = List[int]
-
-stream_buf = bytes()
-stream_counter = 0
-
 audio_rate = 48000
 
 audio_output = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=audio_rate, output=True)
+audio_data = []
 
 
-def recognize(stream_text):
-    global args
-
-    def logger(s):
-        f = open('radio_log.txt', 'a+', encoding='utf-8')
-        f.write(datetime.datetime.now().strftime("[ %d-%b-%Y %H:%M:%S ] "))
-        f.write(s)
-        f.write("\x0A")
-        f.close()
+def signal_handler(sig, frame):
+    global sdr
+    sdr.cancel_read_async()
+    if args.save:
+        save_audio_data_to_file()
+    print("Ctrl+C detectado. Liberando recursos...")
+    
+    sdr.close()
+    
 
 
 def stream_audio(data: AudioStream):
-    global args
-    global stream_buf
-    global stream_counter
+    if args.save:
+        audio_data.append(data)
+    audio_output.write(data)
 
-    if not args.verbose:
-        audio_output.write(data)
-
-    if stream_counter < args.buf:
-        stream_buf += data
-        stream_counter += 1
-    else:
-        threading.Thread(target=recognize, args=(stream_buf,)).start()
-        stream_buf = bytes()
-        stream_counter = 0
-
+def save_audio_data_to_file():
+    filename = f"audio_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.mp3"
+    audio_data_bytes = np.array(audio_data).tobytes()
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(1)  # Número de canales (1 para mono, 2 para estéreo)
+        wf.setsampwidth(2)  # Ancho en bytes de cada muestra
+        wf.setframerate(48000)  # Tasa de muestreo en Hz
+        wf.writeframes(audio_data_bytes)
 
 def process(samples: SampleStream, sdr: RtlSdr) -> None:
     sample_rate_fm = 240000
-    iq_comercial = signal.decimate(samples, int(sdr.get_sample_rate()) // sample_rate_fm)
+    iq_comercial = sg.decimate(samples, int(sdr.get_sample_rate()) // sample_rate_fm)
 
     angle_comercial = np.unwrap(np.angle(iq_comercial))
     demodulated_comercial = np.diff(angle_comercial)
-    audio_signal = signal.decimate(demodulated_comercial, sample_rate_fm // audio_rate, zero_phase=True)
+    audio_signal = sg.decimate(demodulated_comercial, sample_rate_fm // audio_rate, zero_phase=True)
     audio_signal = np.int16(14000 * audio_signal)
 
     stream_audio(audio_signal.astype("int16").tobytes())
 
-
-def read_callback(samples, rtl_sdr_obj):
-    process(samples, rtl_sdr_obj)
-
-
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--ppm', type=int, default=40,
-                    help='ppm error correction')
-parser.add_argument('--gain', type=int, default=10,
-                    help='RF gain level')
-parser.add_argument('--freq', type=int, default=92900000,
+parser.add_argument('--freq', type=int, default=93900000,
                     help='frequency to listen to, in Hertz')
-parser.add_argument('--verbose', action='store_true',
-                    help='mute audio output')
+parser.add_argument('--save', type=bool, default=False,
+                    help='Store audio to file')
 
 args = parser.parse_args()
+sdr =  RtlSdr()
+signal.signal(signal.SIGINT, signal_handler) 
+try:
+    sdr.rs = 240000
+    sdr.fc = args.freq
+    sdr.gain = 'auto'
+    sdr.err_ppm = 40
+    sdr.read_samples_async(process, int(sdr.get_sample_rate()) // 16)
+finally:
+    sys.exit(0)
 
-sdr = RtlSdr()
-sdr.rs = 2400000
-sdr.fc = args.freq
-sdr.gain = args.gain
-sdr.err_ppm = args.ppm
-
-sdr.read_samples_async(read_callback, int(sdr.get_sample_rate()) // 16)
